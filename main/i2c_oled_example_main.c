bf1ecee2c31a648286c4db3ea68ad2dfc9805725
Airat Saetgaraev <saetgaraev.airat@gmail.com>
2026-02-09 14:06:44 +0500

    Изменён текст

--- main/i2c_oled_example_main.c
+++ main/i2c_oled_example_main.c
@@ -55,7 +55,7 @@ void lvgl_demo_ui(lv_display_t *disp)
     lv_obj_t *label = lv_label_create(scr); // Создаем объект метки (текст)
     lv_obj_set_style_text_font(label, &Russian, LV_PART_MAIN); // Устанавливаем русский шрифт для метки
     lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); // Режим циклической прокрутки текста
-    lv_label_set_text(label, "Лось\n228");// Устанавливаем текст с переносом строки (\n)
+    lv_label_set_text(label, "Айрат\n23.09.1996");// Устанавливаем текст с переносом строки (\n)
     /* Размер экрана (если вы используете поворот на 90 или 270, используйте lv_display_get_vertical_solve) */
     lv_obj_set_width(label, lv_display_get_horizontal_resolution(disp));// Устанавливаем ширину метки равной ширине дисплея
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);// Выравниваем метку по верхнему центру экрана
